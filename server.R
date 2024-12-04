9fdf497fdf1ced69698a5fe718084e0563fa84f8
Domnech Finney (South Eastern Sydney LHD) <Domnech.Finney@health.nsw.gov.au>
2024-12-04 14:26:13 +1100

    Refactor shiny app to a minimal viable version.
    Need to enhance some of the renderTables for variety.

--- server.R
+++ server.R
@@ -1,28 +1,87 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
-
-# Define server logic required to draw a histogram
 function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    
+    medication_by_treatment_react = reactive({
+        medications %>%
+            filter(REASONDESCRIPTION == input$Treaments)
+        
+    })
+    procedure_by_treatment_react = reactive({
+        procedures %>%
+            filter(REASONDESCRIPTION == input$Disorder)
+        
+    })
+    
+    
+    
+    # Common medication by disorder
+    output$med_disorder <- renderTable({
+        medication_by_treatment_react() %>%
+            group_by(DESCRIPTION) %>%
+            summarise(Medication = n()) %>%
+            arrange(desc(Medication))
+    })
+    
+    # Common procedure by disorder
+    output$proced_disorder <- renderTable({
+        procedure_by_treatment_react() %>%
+            group_by(DESCRIPTION) %>%
+            summarise(Procedures = n()) %>%
+            arrange(desc(Procedures))
+        
+    })
+    
+    
+    paient_diagnosis_first_date <- reactive({
+        encounters %>%
+            filter(REASONDESCRIPTION == input$Disorder_enc) %>%
+            group_by(PATIENT) %>%
+            arrange(START_POSIX) %>%
+            slice(1) %>%
+            select(PATIENT, diagnosis_date = START_POSIX)
 
     })
+    encounters_prior_to_diagnosis <- reactive({
+        
+        
+        temp1 = encounters %>%
+            left_join(.,
+                      paient_diagnosis_first_date(),
+                      by=c("PATIENT"="PATIENT"))
+        
+        temp2 = temp1 %>%
+            filter(START_POSIX<diagnosis_date) %>%
+            group_by(DESCRIPTION) %>%
+            summarise(Encounter_reason= n()) %>%
+            arrange(desc(Encounter_reason))
 
+    })
+    
+    # Common encounter reasons prior to diagnosis
+    output$prior_encounter_disorder <- renderTable({
+        # paient_diagnosis_first_date()
+        encounters_prior_to_diagnosis()
+    })
+    
+    
+    
+    output$map = renderLeaflet({
+        dynamic_encounter_data = encounter_patient %>%
+            filter(DESCRIPTION == input$Encounter_reason) %>%
+            mutate(count = 1)
+        
+        
+        leaflet(dynamic_encounter_data
+                ) %>%
+            addProviderTiles("CartoDB.Positron") %>%  # Base map
+            addHeatmap(
+                lng = ~LON, 
+                lat = ~LAT, 
+                intensity = ~count,  # Use `count` for heat intensity
+                blur = 20,           # Blurring of points
+                max = 1,            # Maximum intensity
+                radius = 15          # Radius of each point
+            )
+    })
+    
 }
--- ui.R
+++ ui.R
@@ -1,33 +1,55 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define UI for application that draws a histogram
 fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
+    titlePanel("Synthea Demo"),
+    
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            h4("Filters"),
+            conditionalPanel("input.TABS=='Medication_treament'",
+                             selectInput('Treaments',
+                                         choices = sort(unique(na.omit(medications$REASONDESCRIPTION))),
+                                         label = "Treament"
+                                         )
+                             ),
+            conditionalPanel("input.TABS=='Procedure_disorder'",
+                             selectInput('Disorder',
+                                         choices = sort(unique(na.omit(procedures$REASONDESCRIPTION))),
+                                         label = "Disorder"
+                                         )
+                             ),
+            conditionalPanel("input.TABS=='Riskfactor_disorder'",
+                             selectInput('Disorder_enc',
+                                         choices = sort(unique(na.omit(encounters$REASONDESCRIPTION))),
+                                         label = "Encounter reason")
+                             ),
+            conditionalPanel("input.TABS=='map'",
+                             selectInput('Encounter_reason',
+                                         choices = sort(unique(na.omit(encounters$DESCRIPTION))),
+                                         label = "Encounter reason"
+                             )
+            )
         ),
-
-        # Show a plot of the generated distribution
+        
         mainPanel(
-            plotOutput("distPlot")
+            tabsetPanel(id = "TABS", 
+                tabPanel(value = "Medication_treament",
+                         title = "Common medications for disorder",
+                         tableOutput("med_disorder")
+                         ),
+                tabPanel(value = "Procedure_disorder",
+                         title = "Common procedure for disorder",
+                         tableOutput("proced_disorder")
+                         )
+                ,
+                tabPanel(value = "Riskfactor_disorder",
+                         title = "Common encounters prior to disorder",
+                         tableOutput("prior_encounter_disorder")
+                )
+                ,
+                tabPanel(value = "map",
+                         title = "Home address of Patient",
+                         leafletOutput("map")
+                )
+            )
         )
     )
 )
